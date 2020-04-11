650422340be1965681bf85cb3ed72d5ae858b06a
Davide Monfrecola <trampfox@gmail.com>
2020-04-11 14:43:16 +0000

    Add at-rest and in-transit encryption enabled example

--- service/elasticache/examples_test.go
+++ service/elasticache/examples_test.go
@@ -220,6 +220,7 @@ func ExampleElastiCache_CreateCacheCluster_shared00() {
 func ExampleElastiCache_CreateCacheCluster_shared01() {
 	svc := elasticache.New(session.New())
 	input := &elasticache.CreateCacheClusterInput{
+		AtRestEncryptionEnabled:   aws.Bool(false),
 		AutoMinorVersionUpgrade:   aws.Bool(true),
 		CacheClusterId:            aws.String("my-redis"),
 		CacheNodeType:             aws.String("cache.r3.larage"),
@@ -230,6 +231,73 @@ func ExampleElastiCache_CreateCacheCluster_shared01() {
 		Port:                      aws.Int64(6379),
 		PreferredAvailabilityZone: aws.String("us-east-1c"),
 		SnapshotRetentionLimit:    aws.Int64(7),
+		TransitEncryptionEnabled:  aws.Bool(false),
+	}
+
+	result, err := svc.CreateCacheCluster(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case elasticache.ErrCodeReplicationGroupNotFoundFault:
+				fmt.Println(elasticache.ErrCodeReplicationGroupNotFoundFault, aerr.Error())
+			case elasticache.ErrCodeInvalidReplicationGroupStateFault:
+				fmt.Println(elasticache.ErrCodeInvalidReplicationGroupStateFault, aerr.Error())
+			case elasticache.ErrCodeCacheClusterAlreadyExistsFault:
+				fmt.Println(elasticache.ErrCodeCacheClusterAlreadyExistsFault, aerr.Error())
+			case elasticache.ErrCodeInsufficientCacheClusterCapacityFault:
+				fmt.Println(elasticache.ErrCodeInsufficientCacheClusterCapacityFault, aerr.Error())
+			case elasticache.ErrCodeCacheSecurityGroupNotFoundFault:
+				fmt.Println(elasticache.ErrCodeCacheSecurityGroupNotFoundFault, aerr.Error())
+			case elasticache.ErrCodeCacheSubnetGroupNotFoundFault:
+				fmt.Println(elasticache.ErrCodeCacheSubnetGroupNotFoundFault, aerr.Error())
+			case elasticache.ErrCodeClusterQuotaForCustomerExceededFault:
+				fmt.Println(elasticache.ErrCodeClusterQuotaForCustomerExceededFault, aerr.Error())
+			case elasticache.ErrCodeNodeQuotaForClusterExceededFault:
+				fmt.Println(elasticache.ErrCodeNodeQuotaForClusterExceededFault, aerr.Error())
+			case elasticache.ErrCodeNodeQuotaForCustomerExceededFault:
+				fmt.Println(elasticache.ErrCodeNodeQuotaForCustomerExceededFault, aerr.Error())
+			case elasticache.ErrCodeCacheParameterGroupNotFoundFault:
+				fmt.Println(elasticache.ErrCodeCacheParameterGroupNotFoundFault, aerr.Error())
+			case elasticache.ErrCodeInvalidVPCNetworkStateFault:
+				fmt.Println(elasticache.ErrCodeInvalidVPCNetworkStateFault, aerr.Error())
+			case elasticache.ErrCodeTagQuotaPerResourceExceeded:
+				fmt.Println(elasticache.ErrCodeTagQuotaPerResourceExceeded, aerr.Error())
+			case elasticache.ErrCodeInvalidParameterValueException:
+				fmt.Println(elasticache.ErrCodeInvalidParameterValueException, aerr.Error())
+			case elasticache.ErrCodeInvalidParameterCombinationException:
+				fmt.Println(elasticache.ErrCodeInvalidParameterCombinationException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+}
+
+// CreateCacheCluster
+//
+// Creates a Redis cluster with 1 node with at-rest and in-transit encryption enabled.
+func ExampleElastiCache_CreateCacheCluster_shared02() {
+	svc := elasticache.New(session.New())
+	input := &elasticache.CreateCacheClusterInput{
+		AtRestEncryptionEnabled:   aws.Bool(true),
+		AutoMinorVersionUpgrade:   aws.Bool(true),
+		CacheClusterId:            aws.String("my-redis"),
+		CacheNodeType:             aws.String("cache.r3.larage"),
+		CacheSubnetGroupName:      aws.String("default"),
+		Engine:                    aws.String("redis"),
+		EngineVersion:             aws.String("5.0.6"),
+		NumCacheNodes:             aws.Int64(1),
+		Port:                      aws.Int64(6379),
+		PreferredAvailabilityZone: aws.String("us-east-1c"),
+		SnapshotRetentionLimit:    aws.Int64(7),
+		TransitEncryptionEnabled:  aws.Bool(true),
 	}
 
 	result, err := svc.CreateCacheCluster(input)
